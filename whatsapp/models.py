1ee4c3459e1bd3633ae81aa938dd6ec7345766e6
Yuri Caetano <yuuri.caetano@gmail.com>
2023-07-02 17:03:24 -0300

    feat: create whatsapp session model

--- whatsapp/models.py
+++ whatsapp/models.py
@@ -1,4 +1,23 @@
 from django.db import models
+from users.models import Company
+
+class WhatsAppSession(models.Model):
+    """
+    A WhatsAppSession represents a WhatsApp Web session for a company.
+    """
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='whatsapp_sessions')
+    session_token = models.CharField(max_length=255, verbose_name="Token da Sessão")
+    phone_number = models.CharField(max_length=20, verbose_name="Número de Telefone")
+    name = models.CharField(max_length=255, verbose_name="Nome")
+
+    class Meta:
+        db_table = 'whatsapp_sessions'
+        verbose_name = "Sessão do WhatsApp"
+        verbose_name_plural = "Sessões do WhatsApp"
+
+    def __str__(self):
+        return f'Sessão do WhatsApp para {self.company.name} ({self.phone_number})'
+
 
 class Campanha(models.Model):
     name = models.CharField(max_length=200, verbose_name="Nome")
