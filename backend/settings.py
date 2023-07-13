81119e414000a843adfa50e9fd37e6c7c13945e4
Yuri Caetano <yuuri.caetano@gmail.com>
2023-07-13 14:15:17 -0300

    feat: remove the django secret key from settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1mq9k$d%x^s8&t2&%_@&z%ygld#u)!^yk8q_i(oupnnw_06-1e'
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
