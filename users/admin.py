729c3990de7f0c4e50be3e5e2ae16a7c688deb54
Yuri Caetano <yuuri.caetano@gmail.com>
2023-07-18 15:04:18 -0300

    feat: change company admin

--- users/admin.py
+++ users/admin.py
@@ -3,15 +3,26 @@ from .models import Company, Customer, CustomerGroup, Region, Interaction
 from rest_framework.authtoken.models import Token
 from core.auxiliar import AdminBase
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from .models import Company
+
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
     list_display = ('name', 'city', 'state', 'email', 'website')
     search_fields = ('name', 'city', 'state', 'email', 'website')
     readonly_fields = ('owner_token',)
+
     def owner_token(self, obj):
         token, created = Token.objects.get_or_create(user=obj.owner)
         return token.key
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(owner=request.user)
+    
 @admin.register(Customer)
 class CustomerAdmin(AdminBase):
     list_display = ('name', 'whatsapp', 'phone', 'email', 'score')
