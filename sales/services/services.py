6446998e161a54fbea69df72176d1e2320c86a95
Yuri Caetano <yuuri.caetano@gmail.com>
2023-07-19 09:35:44 -0300

    ipdb

--- sales/services/services.py
+++ sales/services/services.py
@@ -40,10 +40,12 @@ class HandleOrderFactory:
 
         products_list_order: List[ProductType] = self.handler.get_order_by_message_id(message_id=self.request.data['message_id'])
         order_manager = OrderManager(self.request, customer, self.handler)
+        ipdb.set_trace()
         if customer.has_order():
             order_manager.update_order(products_list_order)
             return {'status': 'order updated'}
         else:
+        
             order_manager.create_order(products_list_order)
             order_manager.get_categories()
             order_manager.get_recommendations()
