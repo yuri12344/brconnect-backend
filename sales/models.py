cd72d34587c0b22cce35c21d17b0999cc1d48ad4
Yuri Caetano <yuuri.caetano@gmail.com>
2023-09-20 17:34:51 -0300

    chore, add status of order

--- sales/models.py
+++ sales/models.py
@@ -32,7 +32,7 @@ class Order(BaseModel):
         max_length=255, choices=PAYMENT_METHODS_CHOICES, blank=False, null=False, default='P', verbose_name="Metodo de pagamento"
     )
     paid        = models.BooleanField(choices=PAYMENT_CHOICES, default=False, verbose_name="Pago")
-    status      = models.BooleanField(choices=STATUS_CHOICES, default='Enviado', verbose_name="Status")
+    status      = models.CharField(choices=STATUS_CHOICES, default='Não enviado', verbose_name="Status")
     paid_at     = models.DateTimeField(auto_now_add=True, verbose_name="Pago em: ")
     customer    = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='orders', verbose_name="Cliente")
 
