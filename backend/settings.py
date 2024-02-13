5f7a93e2745875b3ec2bb7f095bd1803c7be0eec
Yuri Caetano <yuuri.caetano@gmail.com>
2024-02-13 16:59:17 -0300

    fat: change to mysql

--- backend/settings.py
+++ backend/settings.py
@@ -99,9 +99,17 @@ postgresql = {
         'PORT':     config('DATABASE_PORT'),
 }
 
+mysql = {
+        'ENGINE':   'django.db.backends.mysql',
+        'NAME':     config('DATABASE_NAME'),
+        'USER':     config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST':     config('DATABASE_HOST'),
+        'PORT':     config('DATABASE_PORT'),
+}
 
 DATABASES = {
-    'default': postgresql
+    'default': mysql
 }
 
 
